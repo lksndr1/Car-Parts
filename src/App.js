723ddae27915795cf0192ee173d93c7963d34103
hettfield git <olekpl90@gmail.com>
2024-04-14 18:00:41 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Items from './components/Items';
-import Categories from './components/Categories'
-import { toHaveAccessibleDescription } from '@testing-library/jest-dom/matchers';
+import Categories from './components/Categories';
+import ShowFullItem from './components/ShowFullItem';
 
 class App extends React.Component {
   constructor(props) {
@@ -52,22 +52,32 @@ class App extends React.Component {
           category: 'other',
           price: '27'
         },
-      ]
+      ],
+      showFullItem: false,
+      fullItem : {}
     }
     this.state.currentItems = this.state.items
     this.addToOrder = this.addToOrder.bind(this)
     this.deleteOrder = this.deleteOrder.bind(this)
     this.chooseCategory = this.chooseCategory.bind(this)
+    this.onShowItem = this.onShowItem.bind(this)
   }
   render() {
   return (
     <div className='wrapper'>
       <Header orders={this.state.orders} onDelete = {this.deleteOrder} />
       <Categories chooseCategory = {this.chooseCategory}/>
-      <Items items={this.state.currentItems} onAdd = {this.addToOrder}/>
+      <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd = {this.addToOrder}/>
+
+      {this.state.showFullItem && <ShowFullItem onAdd={this.addToOrder} onShowItem={this.onShowItem} item={this.state.fullItem} />}
       <Footer /> 
     </div>
   );
+  }
+
+  onShowItem(item) {
+    this.setState({fullItem: item})
+    this.setState({showFullItem: !this.state.showFullItem})
   }
 
   chooseCategory(category) {
